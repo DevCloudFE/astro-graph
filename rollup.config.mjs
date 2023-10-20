7f3b9b7c3a17760c5ea5aa6fb896c93331a5873f
huaweidevcloud <devcloudmobile@huawei.com>
2023-10-20 17:13:08 +0800

    feat(build): 补充构建脚本

---
+++ rollup.config.mjs
@@ -0,0 +1,54 @@
+import fs from 'fs';
+import path from 'path';
+import terser from '@rollup/plugin-terser';
+
+export default {
+	input: 'javascript/src/js/mxClient.js',
+	output: [
+		{
+			file: 'javascript/mxClient.js',
+			format: 'cjs',
+			strict: false
+		},
+		{
+			file: 'javascript/mxClient.min.js',
+			format: 'cjs',
+			strict: false,
+			plugins: [terser()]
+		}
+	],
+	plugins: [handleDependencies()]
+}
+
+function handleDependencies() {
+	return {
+		name: 'handleDependencies',
+		transform(content) {
+			const deps = getDependencies(content);
+			const combined = combineDependencies(deps);
+
+			return combined
+		}
+	}
+}
+
+function getDependencies(content) {
+	let deps = content
+		.match(/mxClient\.include\([^"']+["'](.*?)["']/gi)
+		.map(str => str.match(/mxClient\.include\([^"']+["'](.*?)["']/)[1]);
+	deps = ['./js/mxClient.js'].concat(deps.slice(0));
+
+	return deps;
+}
+
+function combineDependencies(dependencies) {
+	let code = '';
+	for (const file of dependencies) {
+		const filePath = path.join(process.cwd(), '/javascript/src/', file);
+		const content = fs.readFileSync(filePath, 'utf-8');
+
+		code += content;
+	}
+
+	return code;
+}
